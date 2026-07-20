1e309b5e414a29bfc4129c80597b1f54da88931f
praful vanker <pmvanker@gmail.com>
2026-07-20 16:27:56 +0530

    update for macbook

---
+++ pmvankerCpp/pmvankerCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pmvankerCpp
+//
+//  Created by Praful Vanker on 19/07/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
